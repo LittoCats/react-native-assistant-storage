e289c34206c5bee5ab21b54acea47491f9ad125a
Littocats <littocats@gmail.com>
2017-05-24 16:32:13 +0800

    不在支持了，不建议使用了

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # react-native-assistant-storage
-Storage Assistant with sqlite3 backend for react-native. 
 
 
-ORM 部分，主要使用了 persistencejs, 但对 persistencejs 中主要的异步方法进行了 Promise 包装。
+not support yet.
--- index.js
+++
@@ -1,7 +0,0 @@
-import KeyChain from './lib/keychain'
-import SQLite3 from './lib/sqlite3'
-import KVStore from './lib/key-value'
-import Persistence from './lib/persistence'
-import PersistenceStoreSqlite3 from './lib/persistence.store.sqlite3'
-
-export {SQLite3, KVStore, Persistence, PersistenceStoreSqlite3}
